b98eca12c3c026ba586b9fcb33e8dde1d27de555
Omar Sahboun <omar.lilian.sahboun@gmail.com>
2022-07-05 09:42:05 +0200

    Ajout fonction ajout_utilisatuer delete_utilisateur update utilisateur et Gui

--- crud.py
+++ crud.py
@@ -10,3 +10,23 @@ def insert_chapter_table(summary):
     connexion.close()
 
 insert_chapter_table()
+
+def ajout_utilisateur(user_name,password):
+    connexion = sqlite3.connect('bdd.db')
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO User  Values (?,?,?);",(None,str(user_name),str(password)))
+    connexion.commit()
+
+def supprime_artiste(id):
+    connexion = sqlite3.connect('bdd.db')
+    curseur = connexion.cursor()
+    curseur.execute("DELETE FROM Artist WHERE userd = ? ;",(id,))
+    connexion.commit()
+
+def maj_nom_artiste(id,nom):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("UPDATE Artist SET Name = ? WHERE ArtistId = ? ;",(nom,id))
+    connexion.commit()
+    connexion.close()
+
---
+++ main.py
@@ -0,0 +1,10 @@
+import crud
+from tkinter import *
+from random import randint
+import sqlite3
+
+root =Tk()
+root.title("Connexion Utilisateur")
+root.geometry("400x400")
+
+root.mainloop()
