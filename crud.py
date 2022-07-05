89befa2505d96840814147a1e1edd013cd752ac5
Marina Supiot <m.v.supiot@gmail.com>
2022-07-05 09:36:56 +0200

    ajout de la fonction caracter

--- crud.py
+++ crud.py
@@ -10,3 +10,14 @@ def insert_chapter_table(summary):
     connexion.close()
 
 insert_chapter_table()
+
+
+
+def creer_caracter(prenom, nom, resume):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO Caracter VALUES (?, ?, ?, ?);", (None, str(prenom), str(nom), str(resume))
+    connexion.commit()
+    connexion.close()
+
+creer_caracter()
