e51ae2294b101277b0bfc47e3d82fa480f4a2cdf
kamel <kamelben9@gmail.com>
2022-07-05 09:23:19 +0200

    ajout des fichiers main et crud(def chapter)

---
+++ crud.py
@@ -0,0 +1,12 @@
+
+import sqlite3
+
+
+def insert_chapter_table(summary):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO Chapter VALUES( ? , ? ) " , (None , summary))
+    connexion.commit()
+    connexion.close()
+
+insert_chapter_table()
