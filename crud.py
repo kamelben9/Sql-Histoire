564a461729a82be7aa0de3a6a91d3434536ded0a
kamel <kamelben9@gmail.com>
2022-07-05 10:18:19 +0200

    ajout des fonctions pour les tables IsInChapter et Challenge

--- crud.py
+++ crud.py
@@ -9,37 +9,40 @@ def insert_chapter_table(summary):
     connexion.commit()
     connexion.close()
 
-insert_chapter_table()
+insert_chapter_table("summary")
 
-<<<<<<< HEAD
 def ajout_utilisateur(user_name,password):
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("INSERT INTO User  Values (?,?,?);",(None,str(user_name),str(password)))
     connexion.commit()
 
-def supprime_artiste(id):
-    connexion = sqlite3.connect('bdd.db')
-    curseur = connexion.cursor()
-    curseur.execute("DELETE FROM Artist WHERE userd = ? ;",(id,))
-    connexion.commit()
-
-def maj_nom_artiste(id,nom):
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("UPDATE Artist SET Name = ? WHERE ArtistId = ? ;",(nom,id))
-    connexion.commit()
-    connexion.close()
-
-=======
-
 
 def creer_caracter(prenom, nom, resume):
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    curseur.execute("INSERT INTO Caracter VALUES (?, ?, ?, ?);", (None, str(prenom), str(nom), str(resume))
+    curseur.execute("INSERT INTO Caracter VALUES (?, ?, ?, ?);", (None, str(prenom), str(nom), str(resume)))
     connexion.commit()
     connexion.close()
 
-creer_caracter()
->>>>>>> 89befa2505d96840814147a1e1edd013cd752ac5
+creer_caracter("test","test","test")
+
+def insert_IsInChapter_table(ChapterId,CaracterId):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO IsInChapter VALUES( ? , ? ) " , (None , None))
+    connexion.commit()
+    connexion.close()
+
+insert_IsInChapter_table(1,1)
+
+
+
+def insert_challenge_table(UserId, ParagraphId, str, int):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO Challenge VALUES( ? , ? , ?, ?) " , (None , None, str, int))
+    connexion.commit()
+    connexion.close()
+
+insert_challenge_table(1,1,"toto",8)
