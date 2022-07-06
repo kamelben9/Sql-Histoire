7ac22c4b0344f4793ed9b4ff3417703180299c6c
kamel <kamelben9@gmail.com>
2022-07-06 10:36:43 +0200

    test

--- fonction.py
+++ fonction.py
@@ -1,1 +1,22 @@
+import hashlib
+
+
+
 #ceci est le fichier pour les fonctions
+
+def verif_user(username, password):
+
+
+    utilisateur = {"username" : None, "password" : None}
+
+    
+    h = hashlib.new('sha256')
+    h.update(password.encode())
+
+    utilisateur["password"] = h.hexdigest()
+
+    print(utilisateur["password"])
+    
+
+    if utilisateur["password"] :
+        pass
